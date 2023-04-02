71099900134790605520202f141d6f015dabf9e4
conguflama <flama000711@icloud.com>
2023-04-02 15:54:15 -0400

    pretrained DINO backbone with a projection head

---
+++ imbalanced_gcd/model.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+
+from torch.nn.init import trunc_normal_
+
+
+class DinoGCD(nn.Module):
+    def __init__(self, out_dim, use_bn=False,
+                 norm_last_layer=True, nlayers=3, hidden_dim=2048, bottleneck_dim=256) -> None:
+        super().__init__()
+        self.out_dim = out_dim
+        # pretrained DINO backbone
+        self.dino = torch.hub.load('facebookresearch/dino:main', 'dino_vitb16')
+        self.embed_len = self.dino.norm.normalized_shape[0]
+        # linear classification head
+        if nlayers == 1:
+            self.mlp = nn.Linear(self.embed_len, bottleneck_dim)
+        else:
+            layers = [nn.Linear(self.embed_len, hidden_dim)]
+            if use_bn:
+                layers.append(nn.BatchNorm1d(hidden_dim))
+            layers.append(nn.GELU())
+            for _ in range(nlayers - 2):
+                layers.append(nn.Linear(hidden_dim, hidden_dim))
+                if use_bn:
+                    layers.append(nn.BatchNorm1d(hidden_dim))
+                layers.append(nn.GELU())
+            layers.append(nn.Linear(hidden_dim, bottleneck_dim))
+            self.mlp = nn.Sequential(*layers)
+        self.apply(self._init_weights)
+        self.last_layer = nn.utils.weight_norm(nn.Linear(bottleneck_dim, out_dim, bias=False))
+        self.last_layer.weight_g.data.fill_(1)
+        if norm_last_layer:
+            self.last_layer.weight_g.requires_grad = False
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        # DINO embeddings
+        raw_embeds = self.dino(x)
+        embeds = raw_embeds.view(raw_embeds.shape[0], -1)
+        # Embeddings after MLP that is used for classification
+        embeds = self.mlp(embeds)
+        embeds = nn.functional.normalize(embeds, dim=-1, p=2)
+        embeds = self.last_layer(embeds)
+        return embeds
