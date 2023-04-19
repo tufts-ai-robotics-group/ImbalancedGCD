1de412cdb7b563b05d73a35658a65c627358c392
conguflama <flama000711@icloud.com>
2023-04-18 22:35:58 -0400

    get rid of DINO normalization for transform

--- imbalanced_gcd/augmentation.py
+++ imbalanced_gcd/augmentation.py
@@ -10,7 +10,7 @@ def dino_norm_trans():
     # DINO normalization for ImageNet pretraining
     return transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     ])
 
 
